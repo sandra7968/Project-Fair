bf95dbe5e65283fffa701003f231dd1cec6d69d4
sandra7968 <sandrapylee@gmail.com>
2023-11-27 13:12:15 +0530

    auth-projects

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,8 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Collapse } from 'react-bootstrap'
+import { BASE_URL } from '../Services/baseurl';
 
 function Profile() {
     const [open, setOpen] = useState(false);
+    const [userProfile,setUserProfile] = useState({
+        username:"",email:"",password:"",profile:"",github:"",linkedin:""
+    })
+    const [existingImage,setExisitingImage] = useState("")
+    const [preview,setPreview] = useState("")
+    useEffect(()=>{
+        const user = JSON.parse(sessionStorage.getItem('existingUser'))
+        if(user.profile!==""){
+            setUserProfile({...userProfile,username:user.username,email:user.email,password:user.password,profile:"",github:user.github,linkedin:user.linkedin})
+            setExisitingImage(user.profile)
+        }else{
+            setExisitingImage("https://cdni.iconscout.com/illustration/premium/thumb/female-user-image-8110250-6515859.png?f=webp")
+        }
+       
+    },[])
+
+    useEffect(()=>{
+        if(userProfile.profile){
+            setPreview(URL.createObjectURL(userProfile.profile))
+        }else{
+            setPreview("")
+        }
+    })
   return (
     <div className='mt-5 '>
         <div className='d-flex border rounded p-3 justify-content-between'>
@@ -12,14 +36,14 @@ function Profile() {
         <Collapse in={open}>
             <div className="row shadow p-5 justify-content-center ">
                 <label className='text-center' >
-                    <input style={{display:'none'}} type="file" />
-                    <img  width={'150px'} height={'150px'} className='rounded-circle bg-dark' src="https://cdni.iconscout.com/illustration/premium/thumb/female-user-image-8110250-6515859.png?f=webp" alt="upload picture" />
+                    <input style={{display:'none'}} type="file" onChange={e=>setUserProfile({...userProfile,profile:e.target.files[0]})} />
+                    <img  width={'150px'} height={'150px'} className='rounded-circle bg-dark' src={preview?preview:`${BASE_URL}/uploads/${existingImage}`} alt="upload picture" />
                 </label>
                 <div className="mt-3">
-                    <input type="text" className='form-control' placeholder='GitHub Link' />
+                    <input type="text" className='form-control' placeholder='GitHub Link' value={userProfile.github} onChange={e=>setUserProfile({...userProfile,github:e.target.value})} />
                 </div>
                 <div className="mt-3">
-                    <input type="text" className='form-control' placeholder='LinkedIn' />
+                    <input type="text" className='form-control' placeholder='LinkedIn' value={userProfile.linkedin} onChange={e=>setUserProfile({...userProfile,linkedin:e.target.value})} />
                 </div>
                 <div className="mt-3 text-center d-grid">
                     <button className='btn btn-warning'>Update</button>
